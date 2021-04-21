aeced9cad603a1fb2d930858563b8d654dded2b6
midori1727 <midori.takaichi@iths.se>
2021-04-21 10:51:38 +0200

    added router.put

--- routes/hamsters.js
+++ routes/hamsters.js
@@ -8,7 +8,6 @@ const db = getDatabase();
 // ** REST API ** 
 
 // Get alla hamsters
-
 router.get('/', async (req, res) => {
 	const hamstersRef = db.collection('hamsters');
 	const snapShot = await hamstersRef.get();
@@ -26,8 +25,8 @@ router.get('/', async (req, res) => {
 	res.send(allHamsters);
 });
 
-// Get random hamsters  /hamsters/random
 
+// Get random hamsters  /hamsters/random
 router.get('/random', async (req, res) => {
 	const hamstersRef = db.collection('hamsters');
 	const snapShot = await hamstersRef.get();
@@ -50,7 +49,6 @@ router.get('/random', async (req, res) => {
 
 
 // Get hamsters med ID  /hamsters/:id
-
 router.get('/:id', async (req, res) => {
 	const id = req.params.id;
 	const docRef = await db.collection('hamsters').doc(id).get();
@@ -64,14 +62,13 @@ router.get('/:id', async (req, res) => {
 	res.send(data);
 
 });
-// POST hamsters        /hamsters
 
+
+// POST hamsters        /hamsters
 router.post('/', async (req, res) => {
 	const object = req.body;
 
-	if
-	// (!object || !object.name || !object.age || !object.favFood || !object.loves || !object.imgName || !object.wins || !object.defeats || !object.games)
-	(!object) {
+	if(!isHamstersObject(object)) {
 		res.sendStatus(400);
 		return;
 	}
@@ -82,8 +79,30 @@ router.post('/', async (req, res) => {
 
 
 // PUT hamsters         /hamsters/:id
+router.put('/:id', async (req, res) => {
+	const object = req.body;
+	const id = req.params.id;
+
+	if(!object || !id) {
+		res.sendStatus(400);
+		return;
+	}
+	
+	const docRef = db.collection('hamsters').doc(id);
+	await docRef.set(object, { merge: true });
+	res.sendStatus(200);
+});
+
+
 // DELETE hamsters      /hamsters/:id
 
 
+// validering
+function isHamstersObject(hamsterObject) {
+	if (!hamsterObject || !hamsterObject.name || !hamsterObject.age || !hamsterObject.favFood || !hamsterObject.loves || !hamsterObject.imgName || !hamsterObject.wins || !hamsterObject.defeats || !hamsterObject.games) {
+		return false;
+	}
+	return true;
+}
 
 module.exports = router;
